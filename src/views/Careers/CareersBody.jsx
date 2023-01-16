63d614cfbe25e7765ed489993ac5d2471128d406
Abdur Razzak <abdur35-2672@diu.edu.bd>
2023-01-17 00:41:14 +0600

    .about team done

--- src/views/Careers/CareersBody.jsx
+++ src/views/Careers/CareersBody.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './career.css'
+import Accordion from 'react-bootstrap/Accordion'
+import { Row,Col } from 'react-bootstrap'
 function CareersBody() {
   return (
     <div>
@@ -19,115 +21,136 @@ function CareersBody() {
         <section className="     m-5">
           <div className=" ">
           <div className="section-title text-center">
-            <p className="section-title__text">Our Values</p>
-            <h2 className="section-title__title">Employee Benefit & <br/>Compensations</h2>
+            <p className="section-title__text">Careers</p>
+            <h2 className=" ">We’re a growing company.</h2>
+
+            <p>Right Time Limited is a leading provider of SOC-as-a-Service (Cyber Security Operations Center – SOC) for the USA,Bangladesh, Australia, and the jarmany Cyber Security market. SafeAeon currently provides security SOC services to many Fortune 1000 companies in collaboration with WaveStrong Inc. and Marseon Inc., which are both US Silicon-Valley based Cyber Security companies.</p>
 
           </div>
-            <ul className="card-wrapper1">
-            <li className="card2">
-                <a className="circleImage mt-4" href="/"><img className="  text-center" src="/assets/images/career/Hybrid.svg" alt="" /></a>
-                <h3 style={{fontSize:"25px"}} className="text-center  my-4" ><a className="text-light" href="">Hybrid/remote/in-office</a></h3>
-            
-              </li>
-              <li className="card2">
-                <a className="circleImage mt-4" href="/"><img className="  text-center" src="/assets/images/career/Paid.svg" alt="" /></a>
-                <h3 style={{fontSize:"25px"}} className="text-center  my-4" ><a className="text-light" href="">Paid vacation
-</a></h3>
-            
-              </li>
-              <li className="card2">
-                <a className="circleImage mt-4" href="/"><img className="  text-center" src="/assets/images/career/Company.svg" alt="" /></a>
-                <h3 style={{fontSize:"25px"}} className="text-center  my-4" ><a className="text-light" href="">Company retreat</a></h3>
-            
-              </li>
-              <li className="card2">
-                <a className="circleImage mt-4" href="/"><img className="  text-center" src="/assets/images/career/Performance.svg" alt="" /></a>
-                <h3 style={{fontSize:"25px"}} className="text-center  my-4" ><a className="text-light" href="/">Performance bonus</a></h3>
-            
-              </li>
-              <li className="card2">
-                <a className="circleImage mt-4" href="/"><img className="  text-center" src="/assets/images/career/Childcare.svg" alt="" /></a>
-                <h3 style={{fontSize:"25px"}} className="text-center  my-4" ><a className="text-light" href="/">Childcare facilities
-</a></h3>
-            
-              </li>
-              <li className="card2">
-                <a className="circleImage mt-4" href="/"><img className="  text-center" src="/assets/images/career/Wellness.svg" alt="" /></a>
-                <h3 style={{fontSize:"25px"}} className="text-center  my-4" ><a className="text-light" href="">Wellness programs</a></h3>
-            
-              </li>
-            
 
-            </ul>
+          <Row>
+            <Col md={4}>
+            <img
+            width="100%"
+              className = "img-responsive"
+              src= "/assets/images/career/job.png"
+              alt=""
+            />
+            </Col>
+
+            <Col md={8}  >
+              <div style={{paddingTop:"80px"}} className='mt-5'>
+                <Accordion defaultActiveKey={['1']} alwaysOpen>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header><h6 >Marketing</h6></Accordion.Header>
+                <Accordion.Body>
+                  <a href='/marketing'>
+                  <h6>Account Based Marketing Executive</h6>
+                  <h6 className='text-info'>Madison, WIS, USA</h6>
+                 </a>
+
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Account Based Marketing Executive</h6>
+                  <h6 className='text-info'>Madison, WIS, Bangladesh</h6>
+                 </a>
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Account Based Marketing Executive</h6>
+                  <h6 className='text-info'>Madison, WIS, Jarmany</h6>
+                 </a>
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Account Based Marketing Executive</h6>
+                  <h6 className='text-info'>Madison, WIS, Australia</h6>
+                 </a>
+                  
+                </Accordion.Body>
+              </Accordion.Item>
+              
+              <Accordion.Item eventKey="2">
+                <Accordion.Header><h6 >Presales Engineering</h6></Accordion.Header>
+                <Accordion.Body>
+                  <a href='/marketing'>
+                  <h6>Senior Presales Engineer</h6>
+                  <h6 className='text-info'>Madison, WIS, USA</h6>
+                 </a>
+
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior Presales Engineer</h6>
+                  <h6 className='text-info'>Madison, WIS, Bangladesh</h6>
+                 </a>
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior Presales Engineer</h6>
+                  <h6 className='text-info'>Madison, WIS, Jarmany</h6>
+                 </a>
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior Presales Engineer</h6>
+                  <h6 className='text-info'>Madison, WIS, Australia</h6>
+                 </a>
+                  
+                </Accordion.Body>
+              </Accordion.Item>
+
+              <Accordion.Item eventKey="3">
+                <Accordion.Header><h6 >R & D</h6></Accordion.Header>
+                <Accordion.Body>
+                  <a href='/marketing'>
+                  <h6>Senior Threat Intelligence Analyst</h6>
+                  <h6 className='text-info'>Madison, WIS, USA</h6>
+                 </a>
+
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior Threat Intelligence Analyst</h6>
+                  <h6 className='text-info'>Madison, WIS, Bangladesh</h6>
+                 </a>
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior Threat Intelligence Analyst</h6>
+                  <h6 className='text-info'>Madison, WIS, Jarmany</h6>
+                 </a>
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior Threat Intelligence Analyst</h6>
+                  <h6 className='text-info'>Madison, WIS, Australia</h6>
+                 </a>
+                  
+                </Accordion.Body>
+              </Accordion.Item>
+ 
+              <Accordion.Item eventKey="4">
+                <Accordion.Header><h6 >Security Service</h6></Accordion.Header>
+                <Accordion.Body>
+                  <a href='/marketing'>
+                  <h6>Senior SOC Analysts</h6>
+                  <h6 className='text-info'>Madison, WIS, USA</h6>
+                 </a>
+
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior SOC Analysts</h6>
+                  <h6 className='text-info'>Madison, WIS, Bangladesh</h6>
+                 </a>
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior SOC Analysts</h6>
+                  <h6 className='text-info'>Madison, WIS, Jarmany</h6>
+                 </a>
+                 <a className='mt-5' href='/marketing'>
+                  <h6>Senior SOC Analysts</h6>
+                  <h6 className='text-info'>Madison, WIS, Australia</h6>
+                 </a>
+                  
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+
+            
+              </div>
+            </Col>
+            
+           </Row>
+    
 
 
           </div>
         </section>
 
 
-      <section className="m-5 pt-5" style = {{backgroundColor :"#261d3b"}}>
-        <div className=" ">
-          <div className="section-title text-center">
-            <p className="section-title__text ">Jobs</p>
-            <h2 className="section-title__title text-light">Opening Jobs</h2>
-          </div>
-          <h4 className ="mx-5">All Jobs</h4>
-          <div className="mx-5 p-5  row border rounded shadow">
-             <div className="col-lg-12">
-              <div className="row">
-                <div className="col-lg-8">
-                  <h1>Junior Our Specialist</h1>
-                  <div className="d-flex">
-                    <h5 style={{marginRight:"30px"}} >Marketing</h5>
-                    <h5>Remote Position</h5>
-                  </div>
-
-                </div>
-                <div className="col-lg-4 jobButton">
-                <a href="/view-job" className="thm-btn cta-two__btn rounded-5">
-              <span>View Position <i className="mx-3 fas fa-arrow-right"></i></span> 
-            </a>
-
-                </div>
-
-              </div>
-             
-                 
-
-            </div>
-          </div>
-
-
-        </div>
-        <br/>
-        <br/>
-
-        <br/>
-
-        <br/>
-
-      </section>
-
-
-      <section className="m-5 pt-5" style = {{backgroundColor :"#261d3b"}}>
-        <div className=" ">
-          <div className="section-title text-center">
-             <h2 className="section-title__title text-light">ReadyTo Build Successfull Tech Product </h2>
-             <h4 className ="mt-3">Schedule a Free Consultation and See How To The Magic Unfolds</h4>
-             <a href="/contact" className=" mt-5 thm-btn cta-two__btn rounded-5">
-                    <span>Start a Project <i className="mx-3 fas fa-arrow-right"></i></span> 
-                  </a>
-
-          </div>
-        
-        </div>
-        <br/>
-        <br/>
-
       
-
-      </section>
-
 
 
 
---
+++ src/views/Company/team/RegionalLeader.js
@@ -0,0 +1,264 @@
+import React,{ useState} from 'react'
+import { Button,Col,Form,Modal, Row } from 'react-bootstrap';
+
+function RegionalLeader() {
+  const [show, setShow] = useState(false);
+
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+  const [show4, setShow4] = useState(false);
+  const [show5, setShow5] = useState(false);
+  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
+  const handleClose3 = () => setShow3(false);
+  const handleShow3 = () => setShow3(true);
+  const handleClose4 = () => setShow4(false);
+  const handleShow4 = () => setShow4(true);
+  const handleClose5 = () => setShow5(false);
+  const handleShow5 = () => setShow5(true);
+
+ 
+
+
+
+
+
+  return (
+    <div>
+       <section className="section-padding--bottom section-padding--top">
+        <div className="container">
+        <h3 className="blog-card-one__title blog-details__title" style={{color:"#17224d",marginTop:"-100px"}} >Regional Leaders</h3>
+
+
+          <div className="body1">
+            <section className="container">
+              <div className="row active-with-click grid">
+                <div className="grid-item">
+                  <article className="material-card Red">
+                    <a onClick={handleShow} href='javascript:void(0)'>
+                       <h2 className="h21 text-light">
+                      <span>Rashed</span>
+                      <strong>
+                      PRESIDENT, RIGHT TIME LIMITED; USA
+                      </strong>
+                     </h2>
+                    </a>
+                   
+                    <div className="mc-content" style={{height:"320px"}} >
+                      <div className="img-container">
+                      <img
+                         className = "img-responsive"
+                          src= "/assets/images/team/Kamruwn Nahar.jpg"
+                          alt=""
+                        />
+                        
+                       </div>
+                      <div className="mc-description mt-3"> PRESIDENT, RIGHT TIME LIMITED; USA</div>
+                    </div>
+                    <a className="mc-btn-action">
+                      <i className="fa fa-bars"></i>
+                    </a>
+                    <div className="mc-footer">
+                      <a href="/facebook" style={{backgroundColor:"#3b5998",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-facebook-f "></a>
+                      <a href="/facebook" style={{backgroundColor:"#00acee",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-twitter"></a>
+                      <a href="/facebook" style={{backgroundColor:"#0A66C2",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-linkedin"></a>
+                      <a href="/facebook" style={{backgroundColor:" #e95950",paddingLeft:"15px",borderRadius:"12px"}} className="fab fa-instagram"></a>
+                    </div>
+                  </article>
+                </div>
+                <div className="grid-item">
+                  <article className="material-card Pink">
+                  <a onClick={handleShow1} href='javascript:void(0)'>
+                    <h2 className="h21 text-light">
+                      <span>Linda</span>
+                      <strong>
+                        <i className="fa fa-fw fa-star"></i>
+                        PRESIDENT, RIGHT TIME LIMITED; GERMANY
+                      </strong>
+                    </h2>
+                    </a>
+                    <div className="mc-content" style={{height:"320px"}} >
+                      <div className="img-container">
+                         <img
+                         className = "img-responsive"
+                         src= "/assets/images/team/Tinene.jpg"
+                         alt=""
+                        />
+                      </div>
+                      <div className="mc-description mt-3">
+                        <p>PRESIDENT, RIGHT TIME LIMITED; GERMANY</p>
+                      </div>
+                    </div>
+                    <a className="mc-btn-action">
+                      <i className="fa fa-bars"></i>
+                    </a>
+                      <div className="mc-footer">
+                      <a href="/facebook" style={{backgroundColor:"#3b5998",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-facebook-f "></a>
+                      <a href="/facebook" style={{backgroundColor:"#00acee",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-twitter"></a>
+                      <a href="/facebook" style={{backgroundColor:"#0A66C2",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-linkedin"></a>
+                      <a href="/facebook" style={{backgroundColor:" #e95950",paddingLeft:"15px",borderRadius:"12px"}} className="fab fa-instagram"></a>
+                    </div>
+                  </article>
+                </div>
+                <div className="grid-item">
+                  <article className="material-card Purple">
+                  <a onClick={handleShow2} href='javascript:void(0)'>
+                    <h2 className="h21 text-light">
+                      <span>Rahim</span>
+                      <strong>
+                        <i className="fa fa-fw fa-star"></i>
+                        PRESIDENT, RIGHT TIME LIMITED; AUSTRALIA
+                      </strong>
+                    </h2>
+                    </a>
+                    <div className="mc-content" style={{height:"320px"}} >
+                      <div className="img-container">
+                      <img
+                         className = "img-responsive"
+                          src= "/assets/images/team/Photdo1.jpeg"
+                          alt=""
+                        />
+                         
+                      </div>
+                      <div className="mc-description mt-3">
+                      PRESIDENT, RIGHT TIME LIMITED; AUSTRALIA
+                      </div>
+                    </div>
+                    <a className="mc-btn-action">
+                      <i className="fa fa-bars"></i>
+                    </a>
+                      <div className="mc-footer">
+                      <a href="/facebook" style={{backgroundColor:"#3b5998",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-facebook-f "></a>
+                      <a href="/facebook" style={{backgroundColor:"#00acee",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-twitter"></a>
+                      <a href="/facebook" style={{backgroundColor:"#0A66C2",paddingLeft:"15px",borderRadius:"15px"}} className="fab fa-linkedin"></a>
+                      <a href="/facebook" style={{backgroundColor:" #e95950",paddingLeft:"15px",borderRadius:"12px"}} className="fab fa-instagram"></a>
+                    </div>
+                  </article>
+                </div>
+
+                
+                 
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
+
+
+
+ 
+ 
+      <Modal   size="lg" show={show} onHide={handleClose}>
+        <Modal.Body  className=" rounded-3">
+          <h5 className='teamTitle'>Rashed</h5>
+           <i style={{ fontSize:"40px"}} className="fab fa-linkedin "></i>
+
+           <Row>
+            <Col md={8}>
+              <p className='teamDetails'>
+              Rashed is in charge  of managing and providing all business verticals  for Right Time Limited to all customers in the US region.              </p>
+ <p>He has over 25+ years of experience conducting security assessments and providing consulting  services to customers in Banking, Financial Services, and Insurance `(BFSI), Payment Industry, and IT Service sector. He   worked for numerous ITES organizations in different regions and countries. He was in charge of completing projects,  including IT security vulnerability assessments, network and application penetration testing, organizational security implementation reviews, and regulatory compliance reviews.</p>
+            </Col>
+
+            <Col md={4}>
+            <img
+              className = "img-responsive"
+              src= "/assets/images/team/Sahaly Yasmwin Bhuiyan.jpg"
+              alt=""
+            />
+
+            </Col>
+             <p>He is officially recognized for his work in ICT network and development. 
+</p>
+           </Row>
+
+
+
+           
+          
+        </Modal.Body>
+       
+      </Modal>
+
+
+      <Modal   size="lg" show={show1} onHide={handleClose1}>
+        <Modal.Body  className=" rounded-3">
+          <h5 className='teamTitle'>Linda</h5>
+           <i style={{ fontSize:"40px"}} className="fab fa-linkedin "></i>
+
+           <Row>
+            <Col md={8}>
+              <p className='teamDetails'>
+              Miss Melinda has over 15 years of operational and decision-making management experience in cultivating and growing international organizations in the various verticals of Information technology enabled Services (ITES) organization. Her area of competence is in implementing information management systems into place in different ITES Organizations.</p>
+ 
+            </Col>
+
+            <Col md={4}>
+            <img
+              className = "img-responsive"
+              src= "/assets/images/team/Mohammad Tohidur Rahsman Bhuieyan.jpg"
+              alt=""
+            />
+
+            </Col>
+            <p>
+Miss Melinda has extensive experience developing, inculcating, and implementing international standards for management systems and achieving operational excellence. She focuses on Systems, People, and Supervision to build high-performance teams that produce sustainable business outcomes.
+</p>
+            </Row>
+
+
+
+           
+          
+        </Modal.Body>
+       
+      </Modal>
+
+
+      <Modal   size="lg" show={show2} onHide={handleClose2}>
+        <Modal.Body  className=" rounded-3">
+          <h5 className='teamTitle'>Rahim</h5>
+           <i style={{ fontSize:"40px"}} className="fab fa-linkedin "></i>
+
+           <Row>
+            <Col md={8}>
+              <p className='teamDetails'>
+              Rahim is in charge  ofall Business, operations, and technical activities, including management and provision  of Right Time Limited services in Australia. </p>
+              <p>Rahim has over 20 years of experience in the security and information technology industry.</p> 
+            </Col>
+
+            <Col md={4}>
+            <img
+              className = "img-responsive"
+              src= "/assets/images/team/Pshoto1.jpeg"
+
+               alt=""
+            />
+
+            </Col>
+            <p>His  knowledge includes performing and managing a variety of IT projects and Audits, Network Security Assessments, Network/Application Security and Architecture Design, and Information Security Technical documentation (Policy, Process, and Guidelines) development projects. </p>
+            <p>He  earned a PhD in Business Management systems from the University of XXX.</p>
+           </Row>
+
+
+
+           
+          
+        </Modal.Body>
+       
+      </Modal>
+
+ 
+
+ 
+      <br/><br/><br/></div>
+  )
+}
+
+export default RegionalLeader
