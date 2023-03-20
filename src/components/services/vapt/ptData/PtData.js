13203d28a4ef81c3b9fc057a80f1976569ebe315
Abdur Razzak <abdur35-2672@diu.edu.bd>
2023-03-21 00:54:10 +0600

    jk

--- src/components/services/vapt/ptData/PtData.js
+++ src/components/services/vapt/ptData/PtData.js
@@ -3,16 +3,16 @@ export const PtData = {
     type_of_pt : [
         {
             id: 1,
-            title: 'Initial study (Business Understanding)',
-            description: 'Perform an initial study of business to understand your card processes, the environment and accordingly combine the PCI scope.',
+            title: ' 	Network Penetration Testing',
+            description: 'Network penetration testing finds and exploits the most exposed vulnerabilities in network infrastructure such as servers, firewalls, and switches. This type of testing can help protect your business from common network-based attacks, such as.',
             image: 'https://www.technovus.com/wp-content/uploads/2019/10/PCI-DSS-Compliance.jpg',
             header:"VAPT"
 
         },
         {
             id: 2,
-            title: '22Scope Finalization ',
-            description: ' Confirm systems that fall under the VAPT scope and articulate the scope statement.',
+            title: ' 	Web Application Penetration Testing ',
+            description: ' It is common for an environment to host a web application that was not specifically coded for the organization such as commercial, off-the-shelf web-mail interfaces, document-sharing tools, file-transfer services, network-device administrative interfaces, etc. In these instances, the web application does not typically need an application-layer penetration test as the entity is not responsible for the source code of this type of software. Instead, the tester should perform a network-layer test and ensure the software was implemented, configured, and is currently being maintained in a secure manner (disabling or uninstalling unused services, blocking unused ports, applying ',
             image: 'https://www.technovus.com/wp-content/uploads/2019/10/PCI-DSS-Compliance.jpg',
             header:"VAPT"
         },
@@ -22,7 +22,7 @@ export const PtData = {
      pt_approach:[
         {
             id: 1,
-            title: 'Initial study (Business Understanding)222',
+            title: ' Security Testing Approaches (based on Information)',
             description: 'Perform an initial study of business to understand your card processes, the environment and accordingly combine the PCI scope.',
             image: 'https://www.technovus.com/wp-content/uploads/2019/10/PCI-DSS-Compliance.jpg',
             header:"VAPT"
@@ -30,7 +30,7 @@ export const PtData = {
         },
         {
             id: 2,
-            title: 'Scope Finalization ',
+            title: ' Analytical Based Penetration Testing',
             description: ' Confirm systems that fall under the VAPT scope and articulate the scope statement.',
             image: 'https://www.technovus.com/wp-content/uploads/2019/10/PCI-DSS-Compliance.jpg',
             header:"VAPT"
@@ -41,16 +41,16 @@ export const PtData = {
     pt_service:[
         {
             id: 1,
-            title: 'Initial study (Business Understanding)',
-            description: 'Perform an initial study of business to understand your card processes, the environment and accordingly combine the PCI scope.',
+            title: ' Manual penetration testing ',
+            description: 'Manual penetration testing is extensive and systematic. It is usually performed by a contractor or security consultancy, who agree on a specific testing scope with the client. Within that scope, an ethical hacker searches for vulnerabilities, attempts to breach the organization’s systems, and prepares a detailed report showing what they discovered and suggesting remediations.',
             image: 'https://www.technovus.com/wp-content/uploads/2019/10/PCI-DSS-Compliance.jpg',
             header:"VAPT"
 
         },
         {
             id: 2,
-            title: 'Scope Finalization ',
-            description: ' Confirm systems that fall under the VAPT scope and articulate the scope statement.',
+            title: 'Automated/Penetration testing as a service (PTaaS ',
+            description: 'Automated/Penetration testing as a service (PTaaS) is a new model, which provides an automated platform organizations can use to perform penetration testing on their systems. PTaaS systems use technologies like automated vulnerability scanning, dynamic application security testing (DAST) and fuzzing, to find security weaknesses and attempt to exploit them, without human intervention.',
             image: 'https://www.technovus.com/wp-content/uploads/2019/10/PCI-DSS-Compliance.jpg',
             header:"VAPT"
         },
