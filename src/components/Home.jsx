0f7db348acd2aa6c07043c0e9f7dd7bbcd9b8cfd
Abdur Razzak <abdur35-2672@diu.edu.bd>
2024-01-30 22:46:19 +0600

    dd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,101 +1,99 @@
 import React from "react";
+import Slider from "react-slick";
 function Home() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
   return (
     <div>
       <div className="page-headser">
         <div
           className="page-hfeader__bg"
         ></div>
-        <div className=" ">
-          <div
-            id="carouselExampleControls"
-            className="carousel slide"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-inner">
-              <div
-                style={{ paddingLeft: "15%",
-                backgroundImage: "url(/assets/images/home/is.png)",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                height: "400px",
-                 width: "100%",
 
-               }}
-                className="carousel-item active"
-              >
-                <ul
-                 className="thm-breadcrumb list-unstyled ">
-                  <li className="pt-5 mt-5">
-                    <a style={{color:"white",fontWeight:"bold"}} href="/">Welcome to Right Time Limited</a>
-                  </li>
-                </ul>
-                <h5 className="page-header__title">
-                  Information Security Services and Solutions
-                </h5>{" "}
-              </div>
-              <div style={{ paddingLeft: "15%",
-              backgroundImage: "url(/assets/images/home/realhack.png)",
+        <Slider {...settings}>
+          <img src="/assets/images/home/is.png" alt=""
+            style={{ height: "400px", width: "100%" }}
+           />
+           <img src="/assets/images/home/realhack.png" alt=""
+            style={{ height: "400px", width: "100%" }}
+           />
+           <img src="/assets/images/home/qsa.png" alt=""
+            style={{ height: "400px", width: "100%" }}
+           />
+
+          {/* <div
+            style={{
+              paddingLeft: "15%",
+              backgroundImage: "url(/assets/images/home/is.png)",
               backgroundSize: "cover",
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
               height: "400px",
-               width: "100%", 
-            
-            }} className="carousel-item">
-                <ul className="thm-breadcrumb list-unstyled ">
-                <li className="pt-5 mt-5">
-                    <a style={{color:"white",fontWeight:"bold"}} href="/">We, Right Time Limited</a>
-                  </li>
-                </ul>
-                <h5 className="page-header__title">Real Hackers !!!</h5>{" "}
-              </div>
-              <div style={{ paddingLeft: "15%" 
-            ,
+              width: "100%",
+
+            }}
+          >
+            <ul
+              className="thm-breadcrumb list-unstyled ">
+              <li className="pt-5 mt-5">
+                <a style={{ color: "white", fontWeight: "bold" }} href="/">Welcome to Right Time Limited</a>
+              </li>
+            </ul>
+            <h5 className="page-header__title">
+              Information Security Services and Solutions
+            </h5>{" "}
+          </div>
+
+
+          <div style={{
+            paddingLeft: "15%",
+            backgroundImage: "url(/assets/images/home/realhack.png)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            height: "400px",
+            width: "100%",
+
+          }}>
+            <ul className="thm-breadcrumb list-unstyled ">
+              <li className="pt-5 mt-5">
+                <a style={{ color: "white", fontWeight: "bold" }} href="/">We, Right Time Limited</a>
+              </li>
+            </ul>
+            <h5 className="page-header__title">Real Hackers !!!</h5>{" "}
+          </div>
+
+          <div style={{
+
             backgroundImage: "url(/assets/images/home/qsa.png)",
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
             height: "400px",
-             width: "100%", }} className="carousel-item">
-                <ul className="thm-breadcrumb list-unstyled ">
-                  <li  className="pt-5 mt-5">
-                    <a style={{color:"white",fontWeight:"bold"}}href="/">Technology</a>
-                  </li>
-                </ul>
-                <h5 className="page-header__title">
-                  First Ever QSA Firm in Bangladesh
-                </h5>{" "}
-              </div>
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
+            width: "100%",
+          }} >
+            <ul className="thm-breadcrumb list-unstyled ">
+              <li className="pt-5 mt-5">
+                <a style={{ color: "white", fontWeight: "bold" }} href="/">Technology</a>
+              </li>
+            </ul>
+            <h5 className="page-header__title">
+              First Ever QSA Firm in Bangladesh
+            </h5>{" "}
           </div>
-      
-        </div>
+ */}
+
+        </Slider>
+
+
+
       </div>
     </div>
   );
